ac774736d9f40cffd95b5486b78ffdc4b40ed8b1
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-11 12:14:56 +0200

    Add funtionality to the links so you can navegate throw the page.

--- daily-page-scripts.js
+++ daily-page-scripts.js
@@ -2,11 +2,22 @@ const enterText = () => {
     const button = $('#add-button');
     const input = $('#input-notes');
     const notes = $('#notes');
+    const todo = $('#todo-list');
+
     button.click(function () {
         const enterNote = input[0].value;
-        const fragment = $('<p></p>');
-        fragment[0].innerText = enterNote;
-        notes.append(fragment);
+        const regEx = /#[A-Za-z].*/gm;
+        if (regEx.test(enterNote)){
+            const fragment = $('<li></li>');
+            fragment[0].innerText = enterNote;
+            todo.append(fragment);
+            input[0].value = "";
+        }else{
+            const fragment = $('<p></p>');
+            fragment[0].innerText = enterNote;
+            notes.append(fragment);
+            input[0].value = "";
+        }
     });
 };
 
