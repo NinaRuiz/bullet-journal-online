ff3740807880c63bfba0d3a01cc79315cc248150
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-18 21:43:16 +0200

    Add div

--- bullet-journal/index-scripts.js
+++ bullet-journal/index-scripts.js
@@ -1,13 +1,29 @@
 const baseUrl = "http://localhost:4000/api/v1/";
 
 //New Notebook Modal
-$('#exampleModal').on('show.bs.modal', function (event) {
-    const submit = $('#createNotebook');
-    submit.click(() => {
-        const input = $('#notebook-name');
-        const name = input[0].value;
-        $.post(baseUrl+'notebook', {name: name}, (response) => {
-            console.log(response);
-        }, 'JSON');
+const createNotebookAction = () => {
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        const submit = $('#createNotebook');
+        submit.click(() => {
+            const input = $('#notebook-name');
+            const name = input[0].value;
+            $.post(baseUrl+'notebook', {name: name}, (response) => {
+                console.log(response);
+            }, 'JSON');
+        });
     });
+};
+
+//Get created notebooks
+const getNotebooks = () => {
+    $.get(baseUrl + 'notebooks', (response) => {
+
+    });
+};
+
+
+//Document ready method
+$(document).ready( () => {
+    createNotebookAction();
+
 });
