aa296376ffd2b0567e94e30fcd496739868634d5
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-19 12:47:10 +0200

    Made pagination work.

--- bullet-journal/index-scripts.js
+++ bullet-journal/index-scripts.js
@@ -21,20 +21,52 @@ const loadPages = () =>{
         let fragment = document.createDocumentFragment();
 
         let previous = document.createDocumentFragment();
-        $(previous).append('<li class="page-item"><a class="page-link" href="#">Previous</a></li>');
+        $(previous).append('<li class="page-item"><button id="previou-button" class="page-link">Previous</button></li>');
 
         let next = document.createDocumentFragment();
-        $(next).append('<li class="page-item"><a class="page-link" href="#">Next</a></li>');
+        $(next).append('<li class="page-item"><button id="next-button" class="page-link">Next</button></li>');
 
         fragment.appendChild(previous);
         for(let i = 1; i <= pages; i++) {
             let link = document.createDocumentFragment();
-            $(link).append('<li class="page-item"><a class="page-link" href="#">'+i+'</a></li>');
+            $(link).append('<li class="page-item"><button class="page-link">'+i+'</button></li>');
             fragment.appendChild(link);
         };
         fragment.appendChild(next);
         $('#pagination').append(fragment);
+        loadPage();
     });
+};
+
+//Load page
+const loadPage = () => {
+    $('.page-link').click( function (e) {
+        e.preventDefault();
+        const pageNumber = this.innerHTML;
+
+        $('#notebooks').html('');
+
+        $.get(baseUrl + 'notebooks/'+ pageNumber, (response) => {
+            $(response.notebooks).each( function() {
+                    const name = this.name;
+                    $('#notebooks').append(
+                        '<div class="card-deck notebook-card">'+
+                        '<div class="card" style="width: 10px;">' +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title">'+ name + '</h5>' +
+                        '<button type="button" class="btn btn-primary" id="notebook-button">Open</button>' +
+                        '<button type="button" class="btn btn-danger" id="delete-notebook-button">Delete</button>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                    );
+                }
+            );
+        });
+
+
+    });
+
 };
 
 //Get created notebooks
